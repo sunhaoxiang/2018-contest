3aacb207b4218381b66a0c805def1d417584589c
孙昊翔 <673686754@qq.com>
2018-04-16 16:39:12 +0800

    框架

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+const Play = () => import('@/views/Play')
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Play',
+      component: Play
     }
   ]
 })
--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <h1>2048 Game</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data () {
+    return {
+    }
+  }
 }
 </script>

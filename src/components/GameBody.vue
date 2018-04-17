648c4bf48b82b055772fe8fbb47f7fc376b038c0
孙昊翔 <673686754@qq.com>
2018-04-17 21:15:59 +0800

    no message

--- src/components/GameBody.vue
+++ src/components/GameBody.vue
@@ -1,32 +1,54 @@
 <template>
   <div class="body-container">
     <div class="grid-container">
-      <grid-cell v-for="(cellPosition, index) of cellPositionList" :cellPosition="cellPosition" :key="index"></grid-cell>
+      <grid-cell
+        v-for="(cellPosition, index) of cellPositionList"
+        :cellPosition="cellPosition"
+        :key="`grid${index}`">
+      </grid-cell>
+      <number-cell
+        v-for="(numberPosition, index) of numberPositionList"
+        :numberPosition="numberPosition"
+        :key="index">
+      </number-cell>
     </div>
   </div>
 </template>
 
 <script>
-const GridCell = () => import('@/components/GridCell')
+import GridCell from '@/components/GridCell'
+import NumberCell from '@/components/NumberCell'
 
 export default {
   components: {
-    GridCell
+    GridCell,
+    NumberCell
   },
   data () {
     return {
+      // board: [[2, 4, 8, 16], [32, 64, 128, 256], [512, 1024, 2048, 4096], [8192, 8192, 8192, 8192]],
       board: [],
-      cellPositionList: []
+      cellPositionList: [],
+      numberPositionList: []
     }
   },
   methods: {
-    // 初始化游戏
+    // 新游戏
     newGame () {
+      // 初始化
       this.init()
+
+      // 随机生成两个数字
+      this.generateNumber()
+      this.generateNumber()
     },
     // 初始化游戏
     init () {
       this.cellPositionList = []
+      this.numberPositionList = []
+      this.board = []
+
+      // 计算出每个网格的位置
       for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
           this.cellPositionList.push({
@@ -35,6 +57,75 @@ export default {
           })
         }
       }
+      // 将board中的值初始化为0
+      for (let i = 0; i < 4; i++) {
+        this.board[i] = []
+        for (let j = 0; j < 4; j++) {
+          this.board[i][j] = 0
+        }
+      }
+      this.updateBoard()
+    },
+    // 更新board数值
+    updateBoard () {
+      let index = 0
+
+      // 计算出每个数字的位置
+      for (let i = 0; i < this.board.length; i++) {
+        for (let j = 0; j < this.board[i].length; j++) {
+          this.numberPositionList.push({
+            index,
+            top: 20 + i * 120,
+            left: 20 + j * 120,
+            value: this.board[i][j]
+          })
+          index++
+        }
+      }
+    },
+    // 在一个随机格子生成数字
+    generateNumber () {
+      // 如果棋盘格没有空间
+      if (this.noSpace()) {
+        return false
+      }
+
+      // 随机生成一个位置
+      let randomX = parseInt(Math.floor(Math.random() * 4))
+      let randomY = parseInt(Math.floor(Math.random() * 4))
+
+      while (true) {
+        if (this.board[randomX][randomY] === 0) {
+          break
+        } else {
+          randomX = parseInt(Math.floor(Math.random() * 4))
+          randomY = parseInt(Math.floor(Math.random() * 4))
+        }
+      }
+
+      // 随机生成一个数字
+      let randomNumber = Math.random() < 0.5 ? 2 : 4
+
+      // 在随机位置显示随机数字
+      this.board[randomX][randomY] = randomNumber
+      // showNumberWithAnimation(randomX, randomY, randomNumber)
+      this.updateBoard()
+
+      return true
+    },
+    // 判断是否可以生成数字
+    noSpace () {
+      for (let i = 0; i < this.board.length; i++) {
+        for (let j = 0; j < this.board[i].length; j++) {
+          if (this.board[i][j] === 0) {
+            return false
+          }
+        }
+      }
+
+      return true
+    },
+    showNumberWithAnimation (i, j, number) {
     }
   }
 }
---
+++ src/components/NumberCell.vue
@@ -0,0 +1,93 @@
+<template>
+  <div
+    v-show="numberPosition.value > 0"
+    class="number-cell"
+    :style="{
+      top: `${numberPosition.top}px`,
+      left: `${numberPosition.left}px`,
+      backgroundColor: colorMap[numberPosition.value].backgroundColor,
+      color: colorMap[numberPosition.value].color
+    }">
+    {{numberPosition.value}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    numberPosition: {
+      type: Object,
+      default: () => ({
+        top: 20,
+        left: 20,
+        value: 0
+      })
+    }
+  },
+  data () {
+    return {
+      colorMap: {
+        0: {
+          backgroundColor: '#fff',
+          color: '#fff'
+        },
+        2: {
+          backgroundColor: '#eee4da',
+          color: '#776e65'
+        },
+        4: {
+          backgroundColor: '#ede0c8',
+          color: '#776e65'
+        },
+        8: {
+          backgroundColor: '#f2b179',
+          color: '#fff'
+        },
+        16: {
+          backgroundColor: '#f59563',
+          color: '#fff'
+        },
+        32: {
+          backgroundColor: '#f67c5f',
+          color: '#fff'
+        },
+        64: {
+          backgroundColor: '#f65e3b',
+          color: '#fff'
+        },
+        128: {
+          backgroundColor: '#edcf72',
+          color: '#fff'
+        },
+        256: {
+          backgroundColor: '#edcc61',
+          color: '#fff'
+        },
+        512: {
+          backgroundColor: '#9c0',
+          color: '#fff'
+        },
+        1024: {
+          backgroundColor: '#33b5e5',
+          color: '#fff'
+        },
+        2048: {
+          backgroundColor: '#09c',
+          color: '#fff'
+        },
+        4096: {
+          backgroundColor: '#a6c',
+          color: '#fff'
+        },
+        8192: {
+          backgroundColor: '#93c',
+          color: '#fff'
+        }
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" src="../assets/scss/numberCell.scss"></style>
